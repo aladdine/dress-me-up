de0745acdea46638ae493e2406495e337ab50013
Alaeddine Mokri <alaeddinemokri@gmail.com>
2015-08-08 13:16:56 -0700

    wrote the Macys API Call

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
       <footer>
         <p>&copy; Company 2015</p>
       </footer>
-    </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    </div> <!-- /container -->        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
         <script src="js/vendor/bootstrap.min.js"></script>
@@ -114,6 +114,8 @@
              $('body').css('background-color','#000');
              $('#scan-item').fadeIn();
           });
+
+
          
 
 
--- scan-item.php
+++ scan-item.php
@@ -1,10 +1,35 @@
 <h3 style="text-align: center;">To Scan, just center a barcoder or QR code between the brackets & hold still.</h3>
-<div class="scanner">
+<div id="scanner">
  <img width="100%"src="img/macy_13.png">
 </div>
 <h3 style="text-align: center;">Or type code here</h3>
 <div class="input-group">
-  <input type="number" class="form-control" placeholder="Or type code here" aria-describedby="basic-addon2">
-  <span class="input-group-addon" id="basic-addon2">Go</span>
+  <input type="text" class="form-control" placeholder="Or type code here" value="749372521125" aria-describedby="basic-addon2">
+  <span class="input-group-addon" id="scan">Go</span>
 </div>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script type="text/javascript">
+	 $("#scan").click(function() {
+
+
+	 $.ajax({
+			url:'http://origin-api.macys.com/v3/catalog/product?upc=50946872827',
+headers: {
+
+'Accept':'application/json',
+
+'X-Macys-Webservice-Client-Id':"atthack2015"
+
+},
+
+			success:function(data) {
+				console.log(data.product[0].image[0].imageurl);
+			}
+
+});
+
+	});
+
+</script>
+
 
