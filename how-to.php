571c60c6949e4de93e56705f25c321f68d8574fe
Alaeddine Mokri <alaeddinemokri@gmail.com>
2015-08-07 23:52:00 -0700

    added the div placeholders

--- how-to.php
+++ how-to.php
@@ -1,13 +1,6 @@
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h1>Macy's <span class="logo">Dress Me Up</span> helps you pair clothing items</h1>
-        <p><a class="btn btn-primary btn-lg" href="#" role="button">Start Shopping &raquo;</a></p>
-      </div>
-    </div>
 
-    <div class="container">
-      <!-- Example row of columns -->
+
+   
       <div class="row">
         <div class="col-md-4">
           <h2>Scan Barcode</h2>
--- index.php
+++ index.php
@@ -48,34 +48,29 @@
         </div><!--/.navbar-collapse -->
       </div>
     </nav>
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
+        <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
         <h1>Macy's <span class="logo">Dress Me Up</span> helps you pair clothing items</h1>
         <p><a class="btn btn-primary btn-lg" href="#" role="button">Start Shopping &raquo;</a></p>
       </div>
     </div>
-
-    <div class="container">
+     <div class="container">
       <!-- Example row of columns -->
-      <div class="row">
-        <div class="col-md-4">
-          <h2>Scan Barcode</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Pair Items</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Check Out</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-      </div>
+
+    <div id="how-to">
+    <? include 'how-to.php'; ?>
+    </div>
+    <div id="scan-item">
+    Enter product code or scan
+    </div>
+    <div id="scan-item">
+    Wait for results to show up here
+    </div>
+    <div id="scan-item">
+    Checkout
+    </div>
+
 
       <hr>
 
