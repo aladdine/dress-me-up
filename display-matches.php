96097845d3bfd4e3c7e3ab6cbb44650cae3b278b
Alaeddine Mokri <alaeddinemokri@gmail.com>
2015-08-08 16:18:46 -0700

    retrieving available sizes and colors of items similar to the one scanned

--- display-matches.php
+++ display-matches.php
@@ -1,1 +1,4 @@
-<p>Loading matches</p>
+<div id="display-matches_image"></div>
+<div id="display-matches_size"></div>
+<div style="width: 100%;"></div>
+<div id="display-matches_color"></div>
--- scan-item.php
+++ scan-item.php
@@ -9,32 +9,70 @@
 </div>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script type="text/javascript">
+
+     
 	 $("#scan").click(function() {
-
-
-	 $.ajax({
-			url:'http://origin-api.macys.com/v3/catalog/product?upc='+ $('#upc').val(),
-headers: {
-
-'Accept':'application/json',
-
-'X-Macys-Webservice-Client-Id':"atthack2015"
-
-},
+	 		$.ajax({
+				url:'http://origin-api.macys.com/v3/catalog/product?upc='+ $('#upc').val(),
+			headers: {
+				'Accept':'application/json',
+				'X-Macys-Webservice-Client-Id':"atthack2015"
+			},
 
 			success:function(data) {
-			//	console.log(data.product[0].image[0].imageurl);
-			console.log(data);
-			$('#scan-item').slideUp();
-			$('body').css('background-color','#FFF');
-			$('#display-matches').show();
-			$('#display-matches').html("<img width='200px' src='"+data.product[0].image[0].imageurl+"'>");
+					console.log(data);
+					$('#scan-item').slideUp();
+					$('body').css('background-color','#FFF');
+					$('#display-matches').show();
+					$('#display-matches_image').prepend("<img width='200px' src='"+data.product[0].image[0].imageurl+"'>");
 
-			}
 
-});
+					$('#display-matches_color').append('<div style="clear: both;"></div>');
+					$('#display-matches_color').append("<p>Select Color:</p>");
+					for (i=0; data.product[0].colorMap.length - 1; ++i ) {
+		                    $('#display-matches_color').append("<div class='sizes' id='color_" + data.product[0].colorMap[i].colornormal + "' style='float: left; width: 25; padding: 3px; height: 25; margin-right: 10px; border: 1px solid #000; background-color:" + data.product[0].colorMap[i].colornormal + ";'></div>");
+		                    if (i>4 && i%5==0) {
+		                    	$('#display-matches_color').append('<div style="clear: both;"></div>');
+		                    }
 
-	});
+					}
+					$('#display-matches_color').append('<div style="clear: both;"></div>');
+					
+                  
+			}, // success function end here
+
+			}); // ajax call ends here
+
+
+             $.ajax({
+				url:'http://origin-api.macys.com/v3/catalog/product?upc='+ $('#upc').val(),
+			headers: {
+				'Accept':'application/json',
+				'X-Macys-Webservice-Client-Id':"atthack2015"
+			},
+
+			success:function(data) {
+		
+                    
+                    $('#display-matches_size').append('<div style="clear: both;"></div>');
+                    $('#display-matches_size').append("<br><p>Select Size:</p>");
+					for (i=0; data.product[0].SizeMap.length - 1; ++i ) {
+		                    $('#display-matches_size').append("<div class='sizes' id='size_" + data.product[0].SizeMap[i].size_value + "' style='float: left; width: 25; padding: 3px; height: 25; margin-right: 10px; border: 1px solid #000; background-color: #ccc;'>" + data.product[0].SizeMap[i].size_value + "</div>");
+		                    if (i>4 && i%5==0) {
+		                    	$('#display-matches_size').append('<div style="clear: both;"></div>');
+		                    }
+
+		                    
+					}
+					$('#display-matches_size').append('<div style="clear: both;"></div>');
+					
+					
+			}, // success function end here
+
+			}); // ajax call ends here
+
+	}); // click event listener ends here
+
 
 </script>
 
